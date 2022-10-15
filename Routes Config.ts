b123c165a4d2032754c1e16a1b79aca85a5d9243
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-10-15 22:50:40 +0100

    Add files via upload

---
+++ Routes Config.ts
@@ -0,0 +1,28 @@
+/**
+ * Define all your routes
+ * @author SoDisliked
+ */
+
+import { Application } from 'express';
+import Locals from './Locals';
+import Log from '../middlewares/Log';
+
+import webRouter from './../routes/Web';
+import apiRouter from './../routes/Api';
+
+class routes {
+    public mountWeb(_express: Application): Application {
+        Log.info('Routes :: Mounting Web Routes...');
+
+        return _express.use('.', webRouter);
+    }
+
+    public mountApi(_express: Application): Application {
+        const apiPrefix = Locals.config().apiPrefix;
+        Log.info('Routes :: Mounting API Routes...');
+
+        return _express.use('${apiPrefix}', apiRouter);
+    }
+}
+
+export default new Routes;
